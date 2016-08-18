8418c4b6ee01ff8e6a534f8656f87a81cdb6d8d9
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2016-08-18 14:12:53 +0000

    Lecture 13

--- lec13/lec13-2.py
+++ lec13/lec13-2.py
@@ -7,7 +7,7 @@ while loc == None:
     time.sleep(0.1)
     loc = pyautogui.locateOnScreen('box.png')
 
-for loc_i in pyautogui.locateAllOnScreen('box.png')
+for loc_i in pyautogui.locateAllOnScreen('box.png'):
     print(loc_i)
     center = pyautogui.center(loc_i)
     pyautogui.click(center)
